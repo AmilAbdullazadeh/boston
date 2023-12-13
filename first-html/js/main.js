40edb036902b3a5cdc060a137dffab552efc6edb
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2023-12-13 11:42:39 +0400

    function convert to arrow func

--- first-html/js/main.js
+++ first-html/js/main.js
@@ -3,12 +3,11 @@ const nav = document.getElementById("nav");
 const menuBg = document.getElementById("menu-bg");
 
 function toggle(element, className) {
-  // debugger
   element.classList.toggle(className);
 }
 
-menuBar.addEventListener("click", toggle(menuBar, "change"));
-nav.addEventListener("click", toggle(nav, "change"));
-menuBg.addEventListener("click", toggle(menuBg, "change-bg"));
-
-//if > else > (class += className), (classList.add, classList.remove)
+menuBar.addEventListener("click", () => {
+  toggle(menuBar, "change");
+  toggle(nav, "change");
+  toggle(menuBg, "change-bg");
+})
