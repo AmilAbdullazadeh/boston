a746ec3a5bcc3c0217a7a74d9495f8d8ea19192d
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2023-12-20 13:05:29 +0400

    form

--- first-html/js/main.js
+++ first-html/js/main.js
@@ -1,13 +1,39 @@
-const menuBar = document.getElementById("menu-bar");
-const nav = document.getElementById("nav");
-const menuBg = document.getElementById("menu-bg");
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+  e.preventDefault();
 
-function toggle(element, className) {
-  element.classList.toggle(className);
-}
+  var message = document.getElementById('message')
 
-menuBar.addEventListener("click", () => {
-  toggle(menuBar, "change");
-  toggle(nav, "change");
-  toggle(menuBg, "change-bg");
+  var firstname = document.getElementById('firstname').value
+  var email = document.getElementById('email').value
+  var password = document.getElementById('password').value
+  var phone = document.getElementById('phone').value
+  var comments = document.getElementById('comments').value
+  var country = document.getElementById('country').value
+
+  var genders = document.querySelectorAll("input[name='gender']:checked")
+  var gender = genders.length > 0 ? genders[0].value : ''
+
+  var interests = document.querySelectorAll("input[name='interests']:checked")
+  var interest = Array.from(interests).map(item => item.value)
+
+  if (
+      firstname.trim() === '' ||
+      email.trim() === '' ||
+      password.trim() === '' ||
+      phone.trim() === '' ||
+      comments.trim() === '' ||
+      country.trim() === '' ||
+      gender.trim() === '' ||
+      interest.length === 0
+  ) {
+    message.textContent = 'Bu saheni bos saxlamayin'
+    message.style.color = 'red'
+    return;
+  }
+
+  if (!email.includes('@')) {
+    message.textContent = 'Daxil edilen format duzgun deyildir'
+    message.style.color = 'red'
+    return;
+  }
 })
