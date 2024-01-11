be75650f65498ae33e5450280f2c589d578f766c
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2024-01-11 11:16:15 +0400

    first api

--- first-html/js/main.js
+++ first-html/js/main.js
@@ -1,98 +1,30 @@
-const arr = [
-  {
-    name: "Lizard",
-    img: "lizard.png",
-  },
-  {
-    name: "Paper",
-    img: "paper.png",
-  },
-  {
-    name: "Rock",
-    img: "rock.png",
-  },
-  {
-    name: "Scissor",
-    img: "scissor.png",
-  },
-  {
-    name: "Spock",
-    img: "spock.png",
-  },
-];
+// import { BASE_URL, DEFAULT_QUERY } from "./config";
+const NASA_API_KEY = "yJwx3NLHdwWnIxvVXHSp7bUyXidnqanuiHaLIeYC";
+const BASE_URL = "https://api.nasa.gov/planetary/apod";
+const DEFAULT_QUERY = `?api_key=${NASA_API_KEY}`;
 
-const imageFolderPath = "assets";
+const title = document.querySelector("#title");
+const date = document.querySelector("#date");
+const picture = document.querySelector("#picture");
+const explanation = document.querySelector("#explanation");
 
-const rules = {
-  Lizard: ["Spock", "Paper"],
 
-  Paper: ["Rock", "Spock"],
-
-  Rock: ["Lizard", "Scissor"],
-
-  Scissor: ["Paper", "Lizard"],
-
-  Spock: ["Scissor", "Rock"],
-};
-
-const playerOptions = document.querySelectorAll(
-  "#player1 .available-options .option"
-);
-
-const botOptions = document.querySelectorAll(
-  "#player2 .available-options .option"
-);
-
-const playerShowArea = document.querySelector(
-  "#player1 .selected-option .option"
-);
-
-const botShowArea = document.querySelector("#player2 .selected-option .option");
-
-const player1Score = document.querySelector("#player1-score");
-const player2Score = document.querySelector("#player2-score");
-
-const roundMessage = document.querySelector("#round-message");
-
-playerOptions.forEach((e) => {
-  e.addEventListener("click", () => {
-    play(e);
-  });
-});
-
-function play(e) {
-  const player = arr[e.dataset.index];
-  const length = arr.length;
-  const botIndex = Math.floor(Math.random() * length);
-  const bot = arr[botIndex];
-
-  showPlayerOption(playerShowArea, player);
-  showPlayerOption(botShowArea, bot);
+// 2. Create a function called fetchNASAData
+const fetchNASAData = async () => {
+  const response = await fetch(`${BASE_URL}${DEFAULT_QUERY}`, {
+    method: "GET"
+  })
+  const data = await response.json()
+  displayData(data)
 }
 
-function generateImgElement(player) {
-  const img = document.createElement("img");
-  img.src = `${imageFolderPath}/${player.img}`;
-  img.alt = `${player.name}`;
-  img.title = `${player.name}`;
-  return img;
+// 3. Call the function
+fetchNASAData()
+
+// 1. Create a function called fetchNASAData
+function displayData(data) {
+  title.textContent = data?.title
+  date.textContent = data.date
+  picture.src = data.hdurl
+  explanation.textContent = data.explanation
 }
-
-function showPlayerOption(showArea, player) {
-  const img = generateImgElement(player);
-
-  showArea.innerHTML = "";
-  showArea.appendChild(img);
-}
-
-// reset
-
-// highlightSelectedOption
-
-// showMessage
-
-//! Claculate function for player 1, player 2 scores (calculateScore with player1, player2)
-
-//! Change the score (addScore with player)
-
-//TODO:: *** confity, alert message, storage score, if the difference is 15 and biger, then reset game and new game start, if I want to add a third player option add it ***
