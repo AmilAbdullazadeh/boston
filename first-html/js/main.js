0232360ed3d4055ca2c4e58a599c64121c2fd355
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2024-01-17 11:42:56 +0400

    jquery first

--- first-html/js/main.js
+++ first-html/js/main.js
@@ -1,66 +1,8 @@
-let form = document.querySelector('#appealsForm');
-let message = document.querySelector('#messageDisplay')
+$(document).ready(function () {
 
-const baseUrl = 'https://jsonplaceholder.typicode.com/posts'
-
-let messageData = {
-    img: '',
-    title: '',
-    content: ''
-}
-
-function visibleMessage() {
-    form.reset()
-
-    form.style.display = 'none'
-    message.style.display = 'block'
-
-    setTimeout(() => {
-        form.style.display = 'block'
-        message.style.display = 'none'
-    }, 5000)
-}
-
-function messageDisplay(data) {
-    let img = document.querySelector('.message-img')
-    let title = document.querySelector('.message-title')
-    let content = document.querySelector('.message-content')
-
-    img.src = data.img
-    title.textContent = data.title
-    content.textContent = data.content
-
-    visibleMessage()
-}
-
-function submit(e) {
-    e.preventDefault()
-
-    let form = this
-    let formData = new FormData(form)
-
-    fetch(baseUrl, {
-        method: 'POST',
-        body: formData
-    }).then(response => {
-        if (response.status === 201) {
-            return response.json()
-        } else {
-            throw new Error('Server wasnt able to process your request')
-        }
+    $(".radio-btn").on("click", function () {
+        $(".radio-inner").toggleClass('active');
+        $("body").toggleClass("dark");
     })
-        .then(data => {
-            messageData.img = './assets/images/success_icon.svg'
-            messageData.title = 'Müraciətiniz uğurla göndərildi!'
-            messageData.content = 'Sizinlə tezliklə əlaqə saxlayacağıq'
-            messageDisplay(messageData)
-        })
-        .catch(error => {
-            messageData.img = './assets/images/error_icon.svg'
-            messageData.title = 'Xəta baş verdi'
-            messageData.content = 'Zəhmət olmasa yenidən cəhd edin'
-            messageDisplay(data)
-        })
-}
 
-form.addEventListener('submit', submit)
+})
