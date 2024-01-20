8e40a1e431ac3cdc4754f281f494a3495785595b
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2024-01-20 11:25:05 +0400

    add ajax

--- first-html/js/main.js
+++ first-html/js/main.js
@@ -1,40 +1,22 @@
 $(document).ready(function () {
 
-    var colors = '0123456789abcdef'
-    var randomColor = '#'
-
-    function generateColor() {
-        for (let i = 0; i < 6; i++) {
-            var randomIndex = Math.floor(Math.random() * 16)
-            randomColor += colors[randomIndex]
-        }
+    function fetchData() {
+        $.ajax({
+            type: "GET",
+            url: 'https://geek-jokes.sr.website/api?format=json',
+            success: (response, statusText) => {
+                $("#joke").text(response.joke)
+            },
+            failure: (err, statusText) => {
+            },
+            error: (err) => {
+                console.log('err', err)
+            }
+        })
     }
 
-    function setColors(color) {
-        $('#inputField').val(color)
-        $('#inputField').css({color: color, borderColor: color})
-        $(".copy-btn").css({backgroundColor: color})
-        $(".color-preview").css({backgroundColor: color})
-    }
+    fetchData()
 
-    function message() {
-        $(".alert-msg").addClass('slide-effect')
-        setTimeout(function() {
-            $(".alert-msg").removeClass('slide-effect')
-        }, 2000)
-    }
+    $("#btn").click(fetchData)
 
-    $(".generate-btn").on("click", function () {
-        generateColor()
-        setColors(randomColor)
-    })
-
-
-    $('.copy-btn').on('click', function () {
-        $("#inputField").select();
-        document.execCommand('copy')
-
-        $('.code').text($('#inputField').val())
-        message()
-    })
-})
+});
