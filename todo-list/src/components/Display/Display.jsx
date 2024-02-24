62c6c0746aa0618aed163fc1a88bd8e7a22ece2d
Amil Abdullazadeh <amil.abdullazadeh@vabiss.com>
2024-02-24 16:20:50 +0400

    test

--- todo-list/src/components/Display/Display.jsx
+++ todo-list/src/components/Display/Display.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos } from '../../actions';
+import List from '../List';
 
 export default function Display() {
   const count = useSelector((state) => state.counter.count);
+  const { loading, items, error } = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
 
-  return (
-      <div>{ count }</div>
-  )
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, [dispatch]);
+
+  if (loading) return <div>Loading ...</div>
+  if (error) return <div>{ error }</div>
+
+  return <List test={items} />;
 }
